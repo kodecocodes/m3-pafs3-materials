d4bd4052945af72b5fd2aac954dabc82e1c4db58
pinalnaik <pinal.naik@gmail.com>
2026-09-23 18:28:05 +0530

    Removed VStack from Shapes.

--- 02-challenge-more-text-styles/Starter/Bullseye/Views/Shapes.swift
+++ 02-challenge-more-text-styles/Starter/Bullseye/Views/Shapes.swift
@@ -2,21 +2,19 @@ import SwiftUI
 
 struct Shapes: View {
   var body: some View {
-    VStack {
-      Circle()
-        .strokeBorder(Color.blue, lineWidth: 20)
-        .frame(width: 200, height: 100)
-        .background(Color.green)
-      RoundedRectangle(cornerRadius: 20)
-        .fill(Color.blue)
-        .frame(width: 200, height: 100)
-      Capsule()
-        .fill(Color.blue)
-        .frame(width: 200, height: 100)
-      Ellipse()
-        .fill(Color.blue)
-        .frame(width: 200, height: 100)
-    }
+    Circle()
+      .strokeBorder(Color.blue, lineWidth: 20)
+      .frame(width: 200, height: 100)
+      .background(Color.green)
+    RoundedRectangle(cornerRadius: 20)
+      .fill(Color.blue)
+      .frame(width: 200, height: 100)
+    Capsule()
+      .fill(Color.blue)
+      .frame(width: 200, height: 100)
+    Ellipse()
+      .fill(Color.blue)
+      .frame(width: 200, height: 100)
   }
 }
 
